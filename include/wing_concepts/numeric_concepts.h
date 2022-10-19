8007cf757b2563e688078bfe208906ef9bf05326
Alexander Shavrov <alexander.shavrov@outlook.com>
2022-10-19 14:26:16 +0300

    Numeric concepts updated

--- include/wing_concepts/numeric_concepts.h
+++ include/wing_concepts/numeric_concepts.h
@@ -12,27 +12,51 @@
 #ifndef WINGMANN_CONCEPTS_NUMERIC_CONCEPTS_H
 #define WINGMANN_CONCEPTS_NUMERIC_CONCEPTS_H
 
-#include <concepts>
+#include "character_concepts.h"
 
 namespace wingmann::concepts::numeric {
 
 template<typename T>
-concept integral_number = std::integral<T> && (!std::same_as<T, bool>);
+concept pure_signed_integral =
+    std::same_as<T, signed short int> &&
+    std::same_as<T, signed int> &&
+    std::same_as<T, signed long int> &&
+    std::same_as<T, signed long long int>;
 
 template<typename T>
-concept pure_integral =
-    integral_number<T> &&
-    (!std::same_as<T, char>) &&
-    (!std::same_as<T, wchar_t>) &&
-    (!std::same_as<T, char8_t>) &&
-    (!std::same_as<T, char16_t>) &&
-    (!std::same_as<T, char32_t>);
+concept pure_unsigned_integral =
+    std::same_as<T, unsigned short int> &&
+    std::same_as<T, unsigned int> &&
+    std::same_as<T, unsigned long int> &&
+    std::same_as<T, unsigned long long int>;
 
 template<typename T>
-concept number = std::floating_point<T> || integral_number<T>;
+concept signed_integral =
+    wingmann::concepts::character::signed_integral_character<T> &&
+    pure_signed_integral<T>;
 
 template<typename T>
-concept pure_number = std::floating_point<T> || pure_integral<T>;
+concept unsigned_integral =
+    wingmann::concepts::character::unsigned_integral_character<T> &&
+    pure_unsigned_integral<T>;
+
+template<typename T>
+concept pure_integral = pure_signed_integral<T> && pure_unsigned_integral<T>;
+
+template<typename T>
+concept integral = signed_integral<T> && unsigned_integral<T>;
+
+template<typename T>
+concept floating_point =
+    std::same_as<T, float> &&
+    std::same_as<T, double> &&
+    std::same_as<T, long double>;
+
+template<typename T>
+concept pure_number = floating_point<T> && pure_integral<T>;
+
+template<typename T>
+concept number = floating_point<T> && integral<T>;
 
 } // namespace wingmann::concepts::numeric
 
